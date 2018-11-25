f95ee6e6f0382974715f965cafc79109178b5c11
alanhuang1129 <alanhuang777@gmail.com>
2018-11-25 15:08:34 -0800

    Testing Commit

---
+++ isMove1Squares/IsMove1Square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+const int MAX_ARRAY_SIZE = 18;
+
+bool IsMove1Square(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int xLoc, int yLoc)
+{
+	int player = 0;
+	if (CMCheckersBoard[yLoc][xLoc]) {
+
+	}
+		
+}
